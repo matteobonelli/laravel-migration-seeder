0a0d1153f76cd6bd5f5ad9fd2ab71fa41a40318a
matteobonelli <matteobonelli08@gmail.com>
2023-12-21 16:01:52 +0100

    trains migration table created

--- database/migrations/2023_12_21_144701_create_trains_table.php
+++ database/migrations/2023_12_21_144701_create_trains_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 255);
+            $table->string('stazione_di_arrivo', 255);
+            $table->dateTimeTz('orario_di_partenza');
+            $table->dateTimeTz('orario_di_arrivo');
+            $table->smallInteger('codice_treno')->unique()->unsigned();
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->tinyInteger('in_orario')->default(1)->unsigned();
+            $table->boolean('cancellato')->deafult(false);
             $table->timestamps();
         });
     }
