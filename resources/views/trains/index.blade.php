b4efed81aa14dc8b433d9e1d6905af9319cb4bb3
matteobonelli <matteobonelli08@gmail.com>
2023-12-21 17:16:26 +0100

    stylized trains departure page

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -7,12 +7,13 @@
 <div class="container">
     <div class="row">
         @foreach($trains as $train)
-        <div class="col-12 d-flex justify-content-center align-items-center">
-            <h6>{{$train->stazione_di_partenza}}</h6>
-            <div>{{$train->orario_di_partenza}}</div>
-            <h6>{{$train->stazione_di_arrivo}}</h6>
-            <div>{{$train->orario_di_arrivo}}</div>
-            <h6>{{$train->codice_treno}}</h6>
+        <div class="col-12 d-flex justify-content-center align-items-center p-2">
+            <h3 class="px-2">TRENO:</h3>
+            <h6 class="px-2">{{$train->stazione_di_partenza}}</h6>
+            <div class="px-2">{{$train->orario_di_partenza}}</div>
+            <h6 class="px-2">{{$train->stazione_di_arrivo}}</h6>
+            <div class="px-2">{{$train->orario_di_arrivo}}</div>
+            <h6 class="px-2">{{$train->codice_treno}}</h6>
         </div>
         @endforeach
     </div>
